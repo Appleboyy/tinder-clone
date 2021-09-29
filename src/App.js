8d67de27725c1a0a2ee435fe4e6c9e1e51a8701c
Appleboyy <weeren97@gmail.com>
2021-09-29 17:17:45 +0800

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import React from 'react';
+import "./Header"
 
 function App() {
   return (
     <div className="app">
       <h1>Testing</h1>
+      <Header />
+      {/* Cards */}
+      {/* Buttons */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="Header">
+            <h2>I am in side Header</h2>
+        </div>
+    )
+}
+
+export default Header
